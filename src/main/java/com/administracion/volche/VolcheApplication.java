3b3fda12ec77ee8c266f92149f97c07f670c0dcc
lab-admin <mariano.a.costanzo@gmail.com>
2018-07-11 11:07:36 -0300

    initial commit

---
+++ src/main/java/com/administracion/volche/VolcheApplication.java
@@ -0,0 +1,12 @@
+package com.administracion.volche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolcheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolcheApplication.class, args);
+	}
+}
